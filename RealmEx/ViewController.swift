0b94d5146f33d522eb9f76689c2f64aa62c5cc75
17rd135 <tbkrk.1112@softbank.ne.jp>
2019-05-18 18:49:15 +0900

    Initial Commit

---
+++ RealmEx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmEx
+//
+//  Created by 津吹陸 on 2019/05/18.
+//  Copyright © 2019 津吹陸. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
